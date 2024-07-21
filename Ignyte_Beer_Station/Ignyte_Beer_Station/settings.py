7faf93e2dc99a4c2d17ef7af3cc74cf030728ea7
Sharan M <101189789+Sharan-m-04@users.noreply.github.com>
2024-07-21 13:04:23 +0530

    Update settings.py

--- Ignyte_Beer_Station/Ignyte_Beer_Station/settings.py
+++ Ignyte_Beer_Station/Ignyte_Beer_Station/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-56&4!hp!l1zo-$gw9ji(=czw=3uw413!i8i@wrc3^p8nr407-x'
+SECRET_KEY = 'YOUR_DJANGO_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
